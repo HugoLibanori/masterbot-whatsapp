a10e1bf7685247d233bdc86cf2bc19c4ec00f662
HugoLibanori <hugolibanori@gmail.com>
2024-02-22 00:29:51 -0300

    erro api removebg

--- src/sticker.ts
+++ src/sticker.ts
@@ -98,7 +98,7 @@ class Stickers {
         return apiKey?.trim();
     }
 
-    public static removerFundoImagem = async (buffer: Buffer, mimetype: string): Promise<string> => {
+    public static removerFundoImagem = async (buffer: Buffer, mimetype: string): Promise<string | undefined> => {
         try {
             const imagemEntradaCaminho: string = path.resolve('media/img/tmp/' + obterNomeAleatorio('.jpg'));
             fs.writeFileSync(imagemEntradaCaminho, buffer);
@@ -140,7 +140,6 @@ class Stickers {
                 'Houve um erro na API REMOVEBG, confira se o limite gratuito da chave excedeu ou se ela está configurada.',
                 'API REMOVEBG',
             );
-            throw new Error();
         }
     };
     public static autoSticker = async (message: any, client: Client): Promise<void> => {
