9116d31af84937a1413f678ba553fec7c8a10ac1
HugoLibanori <hugolibanori@gmail.com>
2024-03-03 18:10:16 -0300

    Corrigindo bemVindo.ts

--- src/bemVindo.ts
+++ src/bemVindo.ts
@@ -6,17 +6,17 @@ export default class BemVindo {
     async bemVindo(client: Client, event: GroupNotification, g_info: any) {
         if (g_info.bemvindo.status) {
             const mentions = [];
-            const id = event.recipientIds.reduce(id => id);
-            const contato = await client.getContactById(id);
-            mentions.push(contato.toString());
-            const grupo = await client.getChatById(event.chatId);
-            const dadosGrupoCast = grupo as unknown;
-            const dadosGrupo = dadosGrupoCast as GroupChat;
-            const title = grupo.name;
-            const msg_customizada = g_info.bemvindo.msg != '' ? g_info.bemvindo.msg + '\n\n' : '';
-            const mensagem_bemvindo = criarTexto(msgs_texto.grupo.bemvindo.mensagem, id.replace('@c.us', ''), title, msg_customizada);
-            await client.sendMessage(event.chatId, mensagem_bemvindo, { mentions });
-            await client.sendMessage(event.chatId, dadosGrupo.description);
+            const idArray = event.recipientIds;
+            for (const id of idArray) {
+                mentions.push(id);
+                const grupo = await client.getChatById(event.chatId);
+                const dadosGrupo = grupo as GroupChat;
+                const title = grupo.name;
+                const msg_customizada = g_info.bemvindo.msg != '' ? g_info.bemvindo.msg + '\n\n' : '';
+                const mensagem_bemvindo = criarTexto(msgs_texto.grupo.bemvindo.mensagem, id.replace('@c.us', ''), title, msg_customizada);
+                await client.sendMessage(event.chatId, mensagem_bemvindo, { mentions });
+                await client.sendMessage(event.chatId, dadosGrupo.description);
+            }
         }
     }
 }
