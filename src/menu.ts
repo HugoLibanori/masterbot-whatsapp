a810b5739b244a9cb9232dc2915dee5fa5b7687e
HugoLibanori <hugolibanori@gmail.com>
2024-03-02 23:10:17 -0300

    UP

--- src/menu.ts
+++ src/menu.ts
@@ -56,7 +56,7 @@ const menu: {
 |- *${PREFIX}tps* - Transforme um TEXTO em sticker.
 |- *${PREFIX}ssf* - Transforme uma IMAGEM em sticker sem fundo.
 |- *${PREFIX}figurinhas* - Envia varias figurinhas para o grupo. obs: funciona só em grupos.
-|- *${PREFIX}salvar* - Salva uma figurinha para ser enviada pelo comando *${PREFIX}figurinhas.
+|- *${PREFIX}salvar* - Salva uma figurinha para ser enviada pelo comando *${PREFIX}figurinhas*.
 
 
 |
