7428a54fcf45cad28d267923c718777be2d37a2f
HugoLibanori <hugolibanori@gmail.com>
2024-03-04 15:37:11 -0300

    Path chrome

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ const client: Client = new Client({
         dataPath: 'session',
     }),
     puppeteer: {
-        executablePath: process.env.PATH_CHROME_WIN || undefined,
+        executablePath: process.env.PATH_CHROME_WIN || process.env.PATH_CHROME_UBUNTU || undefined,
         args: ['--no-sandbox'],
     },
     webVersionCache: {
