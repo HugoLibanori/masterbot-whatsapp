de64402dd89a99eb769e0f5f6ffafab7e8a7cb3f
HugoLibanori <hugolibanori@gmail.com>
2024-03-06 13:49:55 -0300

    anti-porno

--- src/env.ts
+++ src/env.ts
@@ -7,6 +7,8 @@ interface CriacaoEnvResult {
     numero_dono: string;
     newsapi: string;
     apiremovebg: string;
+    apiapornouser: string;
+    apiapornosecret: string;
 }
 
 const criacaoEnv = async (): Promise<void> => {
@@ -43,8 +45,8 @@ const criacaoEnv = async (): Promise<void> => {
         `PATH_CHROME_WIN='C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'\n` +
         `PATH_CHROME_UBUNTU='/usr/bin/google-chrome-stable'\n\n` +
         '############ API SITE https://sightengine.com/ PARA ANTIPORNO ##############\n' +
-        'API_USER=407747126\n' +
-        'API_SECRET=Rer5jezY5yz63BiWaKVa';
+        'API_USER=??????\n' +
+        'API_SECRET=??????';
 
     await fs.writeFile(path.resolve('.env'), envContent, 'utf8');
 };
@@ -53,16 +55,26 @@ const verificarEnv = (): void => {
     const numDono = process.env.NUMERO_DONO ?? 'SEM NUMERO';
     const newsApi = process.env.API_NEWS_ORG ?? 'SEM API_NEWS';
     const apiremovebg = process.env.API_REMOVEBG ?? 'SEM API_REMOVEBG';
+    const apiApornoUser = process.env.API_USER ?? 'SEM API_USER';
+    const apiApornoSecret = process.env.API_SECRET ?? 'SEM API_SECRET';
 
     const resposta: CriacaoEnvResult = {
         numero_dono: numDono.trim() === '55219xxxxxxxx' ? 'O número do DONO ainda não foi configurado' : '✓ Número do DONO configurado.',
         newsapi: newsApi.trim() === '??????' ? 'A API do NEWSAPI ainda não foi configurada' : '✓ API NEWSAPI configurada.',
         apiremovebg: apiremovebg.trim() === '??????' ? 'A API do REMOVEBG ainda não foi configurada' : '✓ API REMOVEBG configurada.',
+        apiapornouser:
+            apiApornoUser.trim() === '??????' ? 'A API ANTI-PORNO USER ainda não foi configurada' : '✓ API ANTI-PORNO USER configurada.',
+        apiapornosecret:
+            apiApornoSecret.trim() === '??????'
+                ? 'A API ANTI-PORNO SECRET ainda não foi configurada'
+                : '✓ API ANTI-PORNO SECRET configurada.',
     };
 
     console.log('[ENV]', corTexto(resposta.numero_dono));
     console.log('[ENV]', corTexto(resposta.newsapi));
     console.log('[ENV]', corTexto(resposta.apiremovebg));
+    console.log('[ENV]', corTexto(resposta.apiapornouser));
+    console.log('[ENV]', corTexto(resposta.apiapornosecret));
 };
 
 export { criacaoEnv, verificarEnv };
